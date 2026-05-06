36e9d5ed4b9363c0a1b6653c262029d1052ac88d
Shubham Patel <snohara340@gmail.com>
2026-05-06 14:40:17 +0530

    add console to find err in auth

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,6 +4,8 @@ const API = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api/auth`,
 });
 
+console.log(import.meta.env.VITE_API_URL);
+
 // 🔥 AUTO ATTACH TOKEN
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
