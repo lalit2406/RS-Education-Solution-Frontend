3e0224000ad1d91f1e6203743bf820597f0d0e7a
Shubham Patel <snohara340@gmail.com>
2026-05-07 21:50:15 +0530

    replace vite config and site logo

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
+
+    sitemap({
+      hostname: "https://rseducationsolution.in",
+    }),
   ],
-})
+});
